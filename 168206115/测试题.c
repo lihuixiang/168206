c22ae0fca8dde45e17878880c42ba1230f4c66e9
lihuixiang <44491961+lihuixiang@users.noreply.github.com>
2018-12-28 16:23:49 +0800

    Update 测试题.c

--- 168206115/测试题.c
+++ 168206115/测试题.c
@@ -1,8 +1,8 @@
-//A说：不是我做的案
-//B说：D是罪犯
-//C说：B是盗窃这块钻石的罪犯
-//D说：B有意诬陷我
-//已知三人说真话，一人假话
+ 1.//A说：不是我做的案
+  //B说：D是罪犯
+  //C说：B是盗窃这块钻石的罪犯
+  //D说：B有意诬陷我
+  //已知三人说真话，一人假话
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@ int  main()
   char  mur = 0;
   for (mur = 'A'; mur <= 'D'; mur++)
   {
-     if ((mur != 'A') + (mur == 'c') +(mur == 'D') + == 3)     //判断谁是罪犯
+     if ((mur != 'A') + (mur == 'D') + (mur == 'B') +（mur ！== 'D')  == 3)     //判断谁是罪犯  三真一假
      {
          print("%c\n",mur);
       }
@@ -21,4 +21,48 @@ int  main()
 }
 
 
- 运行结果：C假话
+ 运行结果：B
+ 
+ 
+ 
+ 
+ 2.//A说：不是我做的案
+  //B说：D是罪犯
+  //C说：B是盗窃这块钻石的罪犯
+  //D说：B有意诬陷我
+  //已知三人说假话，一人说真话
+  
+   #include <stdio.h>
+   #include <string.h>
+   int  main()
+ {
+   char arr[] = {'A','B','C','D'}
+   int i = 0;
+   for (i = 0;i < 4;i++)   //遍历
+   { 
+     if ((arr[i]!='A') + (arr[i]='D') + (arr[i]='B') + (arr[i]!='D') = 1))   //一真三假
+        {print("罪犯是%c\n",arr[i]; //找到罪犯}
+   }
+               system("pause");
+               return 0;
+ }
+     运行结果：罪犯是A
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
